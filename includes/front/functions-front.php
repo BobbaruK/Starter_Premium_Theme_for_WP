c11208bc598b20661cac512189f5303ebbf48c49
Bobbaru <bobbaru_krueger@yahoo.com>
2017-12-29 21:34:05 +0200

    am facut footeru(tags si comments) a lu' standard post format

--- includes/front/functions-front.php
+++ includes/front/functions-front.php
@@ -26,13 +26,38 @@ function csseco_posted_meta() {
 			$i++;
 		}
 	}
-	return '<span class="posted-on">Posted <a href="' . esc_url( get_the_permalink() ) . '">' . $posted_on . '</a> 
-			ago</span> / <span class="posted-in">' . $output . '</span>';
+	return '<span class="posted-on">
+				Posted <a href="' . esc_url( get_permalink() ) . '">' . $posted_on . '</a> ago
+			</span> / 
+			<span class="posted-in">' . $output . '</span>';
 }
 
 /**
  * Generate entry-footer
  */
 function csseco_posted_footer() {
-	return 'tags and comments links';
+	$comments_num = get_comments_number();
+	if ( comments_open() ) {
+		if ( $comments_num == 0 ) {
+			$comments = __( 'No Comments', 'cssecotheme' );
+		} elseif ( $comments_num > 1 ) {
+			$comments = $comments_num . __( 'Comments', 'cssecotheme' );
+		} else {
+			$comments = __( '1 Comment', 'cssecotheme' );
+		}
+		$comments = '<a href="' . get_comments_link() . '">' . $comments . '</a>';
+	} else {
+		$comments = __( 'Comments are closed', 'cssecotheme' );
+	}
+	return '<div class="post-footer-container">
+				<div class="row">
+					<div class="col-xs-12 col-sm-6">
+						' . get_the_tag_list( '<div class="tags-list"><i class="fa fa-tags" aria-hidden="true"></i> ', ', ', '</div>' ) . '
+					</div>
+					<div class="col-xs-12 col-sm-6 text-right">
+						' . $comments . '
+						<i class="fa fa-comments" aria-hidden="true"></i>
+					</div>
+				</div>
+			</div>';
 }
