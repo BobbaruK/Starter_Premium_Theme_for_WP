f0d9c61bf10e055f9005339c15eb5589bc7b212b
Bobbaru <bobbaru_krueger@yahoo.com>
2018-01-24 18:50:18 +0200

    cam asta e... ar mai trebui sa bag mobile detect cu php si cam asta e... mai departe, ce vrea clientu' pfff

--- includes/csseco-variables.php
+++ includes/csseco-variables.php
@@ -3,7 +3,14 @@
  * @package CSSecoThemes
  * csseco-variables.php
  */
-$sidebarLocation = get_option('sidebar_location');
+function csseco_variables_global_init() {
 
-$contentWidth = ( ( get_option('sidebar_location') == 'sidebarLeft' || get_option('sidebar_location') == 'sidebarRight' ) && get_option('content_width') == 12 ? $contentWidth = 9 : ( get_option('sidebar_location') == 'sidebarBottom' || get_option('sidebar_location') == 'sidebarNone' ? 12 : get_option('content_width') ) );
-$sidebarWidth = ( get_option('sidebar_location') == 'sidebarBottom' ? 12 : 12 - $contentWidth );
+	global $sidebarLocation, $contentWidth, $sidebarWidth;
+
+	$sidebarLocation = get_option('sidebar_location');
+
+	$contentWidth = ( ( get_option('sidebar_location') == 'sidebarLeft' || get_option('sidebar_location') == 'sidebarRight' ) && get_option('content_width') == 12 ? $contentWidth = 9 : ( get_option('sidebar_location') == 'sidebarBottom' || get_option('sidebar_location') == 'sidebarNone' ? 12 : get_option('content_width') ) );
+	$sidebarWidth = ( get_option('sidebar_location') == 'sidebarBottom' ? 12 : 12 - $contentWidth );
+
+}
+add_action( 'after_setup_theme', 'csseco_variables_global_init' );
